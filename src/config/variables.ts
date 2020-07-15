4340e5599b45ebb222e6ff725c515d6ff73d60d6
King Elisha <ek.chibueze@gmail.com>
2020-07-15 16:21:18 +0100

    feat: added config variables

---
+++ src/config/variables.ts
@@ -0,0 +1,13 @@
+type Env = "development" | "production";
+const env: Env = process.env.NODE_ENV as Env;
+const port = process.env.PORT || 3000;
+
+export const variables = {
+  env,
+  port,
+  dbPath: "data/db.sqlite3",
+  host:
+    env === "production"
+      ? "https://some.prod.url.com" // TODO: change this
+      : `http://localhost:${port}`,
+};
