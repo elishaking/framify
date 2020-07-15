8fca2c7b276c033527f293d099b62e5bc653cb12
King Elisha <ek.chibueze@gmail.com>
2020-07-15 22:20:01 +0100

    test(addImagesTable): updated migration

--- src/migrations/addImagesTable.ts
+++ src/migrations/addImagesTable.ts
@@ -4,18 +4,21 @@ import { variables } from "../config/variables";
 const db = new Database(variables.dbPath);
 
 export const addImagesTable = () => {
-  db.run(
-    `CREATE TABLE IF NOT EXISTS images (
-        id INTEGER PRIMARY KEY,
-        imageUrl TEXT NOT NULL,
-        size INTEGER NOT NULL,
-        date INTEGER NOT NULL
-    );`,
-    (err) => {
-      if (err) {
-        console.error(err);
+  return new Promise((resolve, reject) => {
+    db.run(
+      `CREATE TABLE IF NOT EXISTS images (
+          id INTEGER PRIMARY KEY,
+          imageUrl TEXT NOT NULL,
+          size INTEGER NOT NULL,
+          date INTEGER NOT NULL
+      );`,
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        db.close();
+        resolve();
       }
-      db.close();
-    }
-  );
+    );
+  });
 };
--- src/tests/routes/image.test.ts
+++ src/tests/routes/image.test.ts
@@ -1,11 +1,22 @@
 import request from "supertest";
 import app from "../../app";
+import { addImagesTable } from "../../migrations/addImagesTable";
 
 describe("Get Endpoints", () => {
+  beforeAll((done) => {
+    addImagesTable().then(() => done());
+  });
+
   it("should get all framed images", async (done) => {
     const res = await request(app).get("/api/image");
-    console.log(res.body);
     expect(res.status).toEqual(200);
+
+    done();
+  });
+
+  it("should return a 404 status for non-existent route", async (done) => {
+    const res = await request(app).get("/api/image/not-a-route");
+    expect(res.status).toEqual(404);
 
     done();
   });
